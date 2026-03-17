dfa9a72c706ce2be2af6a146c2e0b6e22c0fdea2
Cristiano Rodrigues <devcristiano.rodrigues@gmail.com>
2026-03-16 22:53:01 -0300

    fix: fixing null value

--- src/main/java/com/shipay/challenge/client/domain/model/Address.java
+++ src/main/java/com/shipay/challenge/client/domain/model/Address.java
@@ -1,5 +1,7 @@
 package com.shipay.challenge.client.domain.model;
 
+import org.springframework.util.ObjectUtils;
+
 public record Address (
         String zipCode,
         String street,
@@ -8,6 +10,11 @@ public record Address (
         String state
 ){
     public boolean equals(Address address){
+        if (ObjectUtils.isEmpty(address.city()) || ObjectUtils.isEmpty(address.street())
+                || ObjectUtils.isEmpty(address.state())){
+            return false;
+        }
+
         return address.city().equalsIgnoreCase(this.city) &&
                 address.street().equalsIgnoreCase(this.street) && address.state().equalsIgnoreCase(this.state);
     }
